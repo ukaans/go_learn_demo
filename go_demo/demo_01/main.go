39f187c9f49840e260248844ff5e650d84fe89dc
ukaans <2560647580@qq.com>
2026-04-02 20:20:03 +0800

    添加gin，air框架

--- go_demo/demo_01/main.go
+++ go_demo/demo_01/main.go
@@ -1,9 +1,16 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("123123")
+	// 创建一个默认的路由引擎
+	r := gin.Default()
+	// 配置路由
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{ // c.JSON：返回 JSON 格式的数
+			"message": "物质调度系统",
+		})
+	})
+	// 启动HTTP服务，默认在0.0.0.0:8080启动服务
+	r.Run()
 }
