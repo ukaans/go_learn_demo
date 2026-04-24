863836e9d9fbbb434e4c7add441ddbaa21cc6ad4
ukaans <2560647580@qq.com>
2026-04-24 21:05:12 +0800

    模块权限CUDR

--- go_demo/goshopdemo/routers/adminRouters.go
+++ go_demo/goshopdemo/routers/adminRouters.go
@@ -36,5 +36,12 @@ func AdminRoutersInit(r *gin.Engine) {
 		adminRouters.GET("/role/edit", admin.RoleController{}.Edit)
 		adminRouters.POST("/role/doEdit", admin.RoleController{}.DoEdit)
 		adminRouters.GET("/role/delete", admin.RoleController{}.Delete)
+
+		adminRouters.GET("/access", admin.AccessController{}.Index)
+		adminRouters.GET("/access/add", admin.AccessController{}.Add)
+		adminRouters.POST("/access/doAdd", admin.AccessController{}.DoAdd)
+		adminRouters.GET("/access/edit", admin.AccessController{}.Edit)
+		adminRouters.POST("/access/doEdit", admin.AccessController{}.DoEdit)
+		adminRouters.GET("/access/delete", admin.AccessController{}.Delete)
 	}
 }
