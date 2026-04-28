0a4671265aa5ec2f59682c38ac714027353e8cf1
ukaans <2560647580@qq.com>
2026-04-28 16:21:23 +0800

    地址权限判断

--- go_demo/goshopdemo/controllers/admin/main.go
+++ go_demo/goshopdemo/controllers/admin/main.go
@@ -1,8 +1,11 @@
 package admin
 
 import (
+	"encoding/json"
+	"goshopdemo/models"
 	"net/http"
 
+	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,7 +13,48 @@ type MainController struct{}
 
 func (con MainController) Index(c *gin.Context) {
 
-	c.HTML(http.StatusOK, "admin/main/index.html", gin.H{})
+	//获取userinfo 对应的session
+	session := sessions.Default(c)
+	userinfo := session.Get("userinfo")
+	//类型断言 来判断 userinfo是不是一个string
+	userinfoStr, ok := userinfo.(string)
+	if ok {
+		//1.获取用户信息
+		var userinfoStruct []models.Manager
+		json.Unmarshal([]byte(userinfoStr), &userinfoStruct)
+
+		//2.获取用户所有权限
+		accessList := []models.Access{}
+		models.DB.Where("module_id=?", 0).Preload("AccessItem").Find(&accessList)
+
+		//3、获取当前角色拥有的权限 ，并把权限id放在一个map对象里面
+		roleAccess := []models.RoleAccess{}
+		models.DB.Where("role_id=?", userinfoStruct[0].RoleId).Find(&roleAccess)
+		roleAccessMap := make(map[int]int)
+		for _, v := range roleAccess {
+			roleAccessMap[v.AccessId] = v.AccessId
+		}
+
+		//4、循环遍历所有的权限数据，判断当前权限的id是否在角色权限的Map对象中,如果是的话给当前数据加入checked属性
+
+		for i := 0; i < len(accessList); i++ {
+			if _, ok := roleAccessMap[accessList[i].Id]; ok {
+				accessList[i].Checked = true
+			}
+			for j := 0; j < len(accessList[i].AccessItem); j++ {
+				if _, ok := roleAccessMap[accessList[i].AccessItem[j].Id]; ok {
+					accessList[i].AccessItem[j].Checked = true
+				}
+			}
+		}
+		c.HTML(http.StatusOK, "admin/main/index.html", gin.H{
+			"username":   userinfoStruct[0].Username,
+			"accessList": accessList,
+			"isSuper":    userinfoStruct[0].IsSuper,
+		})
+	} else { //没有登录
+		c.Redirect(302, "/admin/login")
+	}
 
 }
 
--- go_demo/goshopdemo/middlewares/adminAuth.go
+++ go_demo/goshopdemo/middlewares/adminAuth.go
@@ -4,15 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"goshopdemo/models"
+	"os"
 	"strings"
 
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
+	"gopkg.in/ini.v1"
 )
 
 func InitAdminAuthMiddleware(c *gin.Context) {
 
-	fmt.Println("InitAdminAuthMiddleware")
 	//进行权限判断 没有登录的用户 不能进入后台管理中心
 
 	//1、获取Url访问的地址  /admin/captcha
@@ -34,11 +35,33 @@ func InitAdminAuthMiddleware(c *gin.Context) {
 	if ok {
 		var userinfoStruct []models.Manager
 		err := json.Unmarshal([]byte(userinfoStr), &userinfoStruct)
-
 		if err != nil || !(len(userinfoStruct) > 0 && userinfoStruct[0].Username != "") {
 			if pathname != "/admin/login" && pathname != "/admin/doLogin" && pathname != "/admin/captcha" {
 				c.Redirect(302, "/admin/login")
 			}
+		} else { //用户登录成功 权限判断
+			urlPath := strings.Replace(pathname, "/admin/", "", 1)
+
+			if userinfoStruct[0].IsSuper == 0 && !excludeAuthPath("/"+urlPath) {
+
+				// 1、根据角色获取当前角色的权限列表,然后把权限id放在一个map类型的对象里面
+				roleAccess := []models.RoleAccess{}
+				models.DB.Where("role_id=?", userinfoStruct[0].RoleId).Find(&roleAccess)
+				roleAccessMap := make(map[int]int)
+				for _, v := range roleAccess {
+					roleAccessMap[v.AccessId] = v.AccessId
+				}
+				// 2、获取当前访问的url对应的权限id 判断权限id是否在角色对应的权限
+				// pathname   /admin/manager
+				access := models.Access{}
+				models.DB.Where("url = ?", urlPath).Find(&access)
+				//3、判断当前访问的url对应的权限id 是否在权限列表的id中
+				if _, ok := roleAccessMap[access.Id]; !ok {
+					c.String(200, "没有权限")
+					c.Abort()
+				}
+			}
+
 		}
 	} else { //没有登录
 		if pathname != "/admin/login" && pathname != "/admin/doLogin" && pathname != "/admin/captcha" {
@@ -46,4 +69,25 @@ func InitAdminAuthMiddleware(c *gin.Context) {
 		}
 	}
 
+}
+
+//排除权限判断的方法
+
+func excludeAuthPath(urlPath string) bool {
+	config, iniErr := ini.Load("./conf/app.ini")
+	if iniErr != nil {
+		fmt.Printf("Fail to read file: %v", iniErr)
+		os.Exit(1)
+	}
+	excludeAuthPath := config.Section("").Key("excludeAuthPath").String()
+
+	excludeAuthPathSlice := strings.Split(excludeAuthPath, ",")
+	// return true
+	fmt.Println(excludeAuthPathSlice)
+	for _, v := range excludeAuthPathSlice {
+		if v == urlPath {
+			return true
+		}
+	}
+	return false
 }
